0fb75555bbfa4ca9c42bdebc7b958ffd1fe87d8e
Gianvito Forte <gianvy09@gmail.com>
2025-05-21 14:35:48 +0200

    Fix CORS per dominio Vercel

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ import houseRoutes from './routes/house.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://shared-frontend.vercel.app'
+    ],
+    credentials: true
+}));
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/house', houseRoutes);
