5c47d5c141cbcabb6432434e395b4872cc9aa830
Gianvito Forte <gianvy09@gmail.com>
2025-05-21 17:26:36 +0200

    speriamo ultimo fix per cross platform

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,11 @@ router.post('/login', async (req, res) => {
   const user = await User.findOne({ email });
   if (!user || !(await bcrypt.compare(password, user.password))) return res.sendStatus(401);
   const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET);
-  res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None'
+  }).sendStatus(200);
 });
 
 router.post('/logout', (req, res) => {
@@ -27,4 +31,5 @@ router.post('/logout', (req, res) => {
 });
 
 export default router;
+
 
